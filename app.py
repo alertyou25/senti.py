84e542323db36f11a12e52180bb9e32ea2e284ae
alertyou25 <gaurikasavi@gmail.com>
2025-04-30 17:00:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return " API is working"
+    return "API is working"
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    data = request.get_json()
-    text = data.get("text", "")
-    if not text:
-        return jsonify({"error": "No text provided"}), 400
+    data = request.get_json()
+    text = data.get("text", "")
+    if not text:
+        return jsonify({"error": "No text provided"}), 400
 
-    blob = TextBlob(text)
-    polarity = blob.sentiment.polarity
-    return jsonify({"sentiment": polarity})
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    return jsonify({"sentiment": polarity})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3000)
